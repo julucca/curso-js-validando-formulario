9435cdb27030a51d1ddc067f898a89ee4ffc89c6
julucca <lucca.editora@gmail.com>
2022-11-01 09:50:17 -0300

    Aula 2 - validação com JS

---
+++ js/script.js
@@ -0,0 +1,13 @@
+import ehUmCPF from "./valida-cpf.js";
+
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
+})
+
+function verificaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCPF(campo);
+    }
+}
